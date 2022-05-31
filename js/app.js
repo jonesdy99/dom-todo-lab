807fdc3547d6f23cfdbd1770d759474d0b8398dc
Jonesdy99 <jonesdylan1165@gmail.com>
2022-05-31 17:07:32 -0400

    add button function that allows user to remove object

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 const addBtn = document.getElementById('add-button')
 const submitBtn = document.getElementById('submit-button')
 const resetBtn = document.getElementById('reset-button')
-const toDoList = document.getElementById('todo-list')
+const todoList = document.getElementById('todo-list')
 
 
 submitBtn.addEventListener('click', function(evt){
-  if (addBtn.value === '')
-  return const newTodo = document.createElement('li')
+  if (addBtn.value === '')return 
+  const newTodo = document.createElement('li')
   newTodo.textContent = addBtn.value
   todoList.appendChild(newTodo)
   addBtn.value = ''
+})
+
+todoList.addEventListener('click', function(evt){
+  const parentNode = evt.target.parentNode
+  parentNode.removeChild(evt.target)
 })
